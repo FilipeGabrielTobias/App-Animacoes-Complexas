732242b2ef287368a47ec7050b0ec4a86cf4dee6
Filipe Tobias <filipe.tobias@senior.com.br>
2022-03-16 21:59:15 -0300

    Criando a tela de login

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,48 @@
+import 'package:animacoes_complexas/screens/login/widgets/form_container.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.jpg'),
+            fit: BoxFit.cover
+          )
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 70, bottom: 32),
+                      child: Image.asset(
+                        "images/tickicon.png", 
+                        width: 150,
+                        height: 150,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    FormContainer()
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
